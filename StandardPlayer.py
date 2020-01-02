62779a06733a2878ca781442211813db9a3cc40e
Gabriel Kressin <gabriel2000x@hotmail.de>
2020-01-03 00:23:48 +0100

    Added Sceleton for Basic Player

---
+++ StandardPlayer.py
@@ -0,0 +1,12 @@
+# The very Basic Player for a 4wins game
+# Author: Gabriel Kressin
+
+import random
+
+class StandardPlayer:
+    ''' A basic 4wins player '''
+
+    def __init__(self):
+        ''' Init function '''
+
+        self.name = "Standard Player"
