b12f8d6eb650ff0fae1cb499b375f9f8393e8f17
Gabriel Kressin <gabriel2000x@hotmail.de>
2020-01-03 00:24:48 +0100

    Added Matrix, fullness, Getter and PlayerInit

---
+++ 4win.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+# 4 wins
+# Authors:
+# Gabriel Kressin
+
+import sys, copy, random, importlib
+
+class FourWins:
+    ''' Includes all Game functions for 4 wins '''
+
+    def __init__(self, playerName1="standard", playerName2="standard"):
+        ''' Initializes the Game '''
+        # Set Variables
+
+        # The single underscore means you should NOT access those variables!
+        # To get the matrix Data use respective getter and setter functions!
+        self._matrix = self.matrixCreate()
+        self._fullness = self.fullnessCreate()
+
+        # Init players
+        self.player1 = self.playerInit(playerName1)
+        self.player2 = self.playerInit(playerName2)
+
+        self.playerOnTurn()
+
+
+    def matrixCreate(self, cols=7, rows=7):
+        ''' The Matrix shows where which stone is '''
+        # This is how it is adressed:
+        # [0][0] [0][1] [0][2] ... [0][6]
+        # [1][0] ...
+        # ...
+        # [6][0] ...           ... [6][6]
+        return [[0] * cols for i in range(rows)]
+
+    def matrixGet(self):
+        ''' Returns a copy of the Matrix '''
+        return copy.deepcopy( self.matrix )
+
+    def matrixGetStone(self, col, row):
+        ''' Returns the stone in specific row and column '''
+        return self.matrix[col][row]
+
+    def fullnessCreate(self, cols=7):
+        ''' The fullness shows how many stones are in a column of the matrix '''
+        return [0] * cols
+
+    def fullnessGet(self):
+        ''' Returns a copy of the fullness '''
+        return copy.copy( self.fullness )
+
+    def playerInit(self, playerName):
+        ''' Initializes and imports Player Object, respective player file needs to be included above '''
+
+        # Import and return Standard Player
+        if playerName == "standard":
+            mod = importlib.import_module( "StandardPlayer" )
+            return mod.StandardPlayer()
+        # Import and return an Interactive Player
+        elif playerName == "interactive":
+            mod = importlib.import_module( "InteractivePlayer" )
+            return mod.InteractivePlayer()
+        # Import and return Example Player
+        elif playerName == "exampleName":
+            # Copy this Code and adapt it to your player
+            mod = importlib.import_module( "examplePlayer" )
+            return mod.ExamplePlayer()
+        # In Case of not finding a Player end execution
+        else:
+            print( "No such player {}, using standard player".format(playerName) )
+            sys.exit(-1)
+
+
+
+def main():
+    fwins = FourWins()
+
+if __name__ == "__main__":
+    main()
