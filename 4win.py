296030c081dd9a4b631e022d2182a2e3c76c9c75
Gabriel Kressin <gabriel2000x@hotmail.de>
2020-01-03 12:15:53 +0100

    fixed row/col order

--- 4win.py
+++ 4win.py
@@ -102,8 +102,8 @@ class FourWins:
 
     def _placeStone(self, pos):
         ''' Places a Stone on pos in matrix '''
-        self._matrix[pos][self._fullness[pos]] = self._currentPlayer
-        self._lastStone = (pos, self._fullness[pos])
+        self._matrix[self._fullness[pos]][pos] = self._currentPlayer
+        self._lastStone = (self._fullness[pos], pos)
         self._fullness[pos] += 1
 
 
