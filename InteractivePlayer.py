756db2095fb665d31bc01a1c7f857683da321f4e
Gabriel Kressin <gabriel2000x@hotmail.de>
2020-01-03 19:20:12 +0100

    InteractivePlayer

---
+++ InteractivePlayer.py
@@ -0,0 +1,26 @@
+# Interactive Player for the 4wins game
+# Author: Gabriel Kressin
+
+class InteractivePlayer:
+    ''' A player to allow a human to play the 4wins game '''
+
+    def __init__(self, game):
+        ''' Init function '''
+
+        self.name = "Interactive Player"
+        self.game = game
+
+
+    def nextTurn(self):
+        ''' Returns col in which the next stone should be placed '''
+        newPos = -1
+        wrongInput = False
+
+        while not game.moveLegal(newPos):
+            # Get User Input
+            if wrongInput:
+                print( "{} is nnot a valid Input! Please type an integer between 0 and 6 in a column which is not full!".format(newPos) )
+            newPos = input( "Place a Stone! (type an integer [0,6])" )
+            wrongInput = True
+
+        return newPos
