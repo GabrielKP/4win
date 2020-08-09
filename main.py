80095786aaba587177e369cbd789e688bbf6338c
Gabriel Kressin <gabriel.kressin@fu-berlin.de>
2020-08-09 17:31:53 +0200

    little fixes

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class FourWins:
     def __init__( self, p1, p2, verbose ):
         """
         Initialize the board, set the variables
-        p1/p2 are functions which get the board, the fullness and last history
+        p1/p2 are functions which get the board, the height and last history
         """
 
         self._p1 = p1
@@ -20,6 +20,7 @@ class FourWins:
         self._verbose = verbose
         self._HEIGHT = 6
         self._WIDTH = 7
+        self._H1 = self._HEIGHT + 1
         self._moves = [] * self._SIZE
         self._SIZE = self._HEIGHT * self._WIDTH
         self._reset()
@@ -31,7 +32,7 @@ class FourWins:
         """
         self._board = [0] * 2
         self._turns = 0
-        self._fullness = [0] * self._WIDTH
+        self._height = [ self._H1 * i for i in range( self._HEIGHT ) ]
 
 
     def _makemove( self, column ):
@@ -39,10 +40,17 @@ class FourWins:
         Places a stone in column
         """
         if self.islegal( column ):
-            self._board[self._turns & 1] = self._fullness[column]
+            self._board[self._turns & 1] = self._height[column]
 
 
     def islegal( self, column ):
         """
         Checks if stone placement is legal
         """
+
+def main():
+    print( "Hello World!" )
+
+
+if __name__ == "__main__":
+    main()
